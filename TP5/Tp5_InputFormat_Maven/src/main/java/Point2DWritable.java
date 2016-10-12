6171e2c2321b056be6de30d5ae1e55cde76d5b02
Nzaoui <naji.zaoui@hotmail.fr>
2016-10-12 18:42:37 +0200

    Depot TP5

---
+++ TP5/Tp5_InputFormat_Maven/src/main/java/Point2DWritable.java
@@ -0,0 +1,67 @@
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+import org.apache.hadoop.io.Writable;
+
+
+/**
+ * The class Point2DWritable is what would be used by the mapper and the reducer
+ * it's the same as IntWritable that writes an int, but instead here we will be
+ * writing a Point2D.
+ *
+ * We implement the class Writable to use the methods:
+ *  - write(DataOutput) to write the instance (a Point2D).
+ *  - readFields(DataInput) to read an instance (the two points of the Point2D).
+ *  
+ *  N.B: Take a look a look at {@link Writable} for further informations.
+ *  
+ */
+public class Point2DWritable implements Writable {
+
+	private double x;
+	private double y;
+
+	public Point2DWritable() {
+
+	}
+
+	public Point2DWritable(double a, double b) {
+		setX(a);
+		setY(b);
+	}
+
+	public String ToString() {
+		return "(" + this.x + "," + this.y + ")";
+	}
+
+	public void write(DataOutput out) throws IOException {
+
+		out.writeDouble(x);
+		out.writeDouble(y);
+	}
+
+	public void readFields(DataInput in) throws IOException {
+
+		x = in.readDouble();
+		y = in.readDouble();
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+}
